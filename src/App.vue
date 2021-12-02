abc1981d1590b1afc0bec7e72cad7db78d3f7b4e
Christie Maduna <ferggiee@gmail.com>
2021-12-02 10:13:17 +0200

    comit

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,13 @@ const signIn = () => {
   console.log("Want to sign in");
 };
 
-const email = () => {
-  console.log("enter email address");
-};
+// const email = () => {
+//   console.log("enter email address");
+// };
 
-const password = () => {
-  console.log("enter password");
-};
+// const password = () => {
+//   console.log("enter password");
+// };
 </script>
 
 <template>
@@ -32,7 +32,9 @@ const password = () => {
       <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
         <form class="space-y-6" action="#" method="POST">
           <div>
-            <base-text-input id="email">email</base-text-input>
+            <base-text-input inputType="email" autocomplete="email" required
+              >email</base-text-input
+            >
             <!-- <label for="email" class="block text-sm font-medium text-gray-700">
               Email address
             </label>
@@ -47,27 +49,6 @@ const password = () => {
               />
             </div> -->
           </div>
-
-          <div>
-            <base-text-input id="password">password</base-text-input>
-            <!-- <label
-              for="password"
-              class="block text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-            <div class="mt-1">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autocomplete="current-password"
-                required="true"
-                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              />
-            </div> -->
-          </div>
-
           <div class="flex items-center justify-between">
             <div class="flex items-center">
               <input
--- src/components/baseComponents/BaseTextInput.vue
+++ src/components/baseComponents/BaseTextInput.vue
@@ -1,27 +1,26 @@
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  inputType: String,
+  autocomplete: String,
+  required: Boolean,
+});
+</script>
+
 <template>
-  <label for="email" class="block text-sm font-medium text-gray-700"> </label>
+  <label :for="inputType" class="block text-sm font-medium text-gray-700">
+  </label>
   <div class="mt-1">
     <input
-      id="email"
-      name="email"
-      type="email"
-      autocomplete="email"
-      required="true"
+      :id="inputType"
+      :name="inputType"
+      :type="inputType"
+      :autocomplete="autocomplete"
+      :required="required"
       class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
     />
   </div>
 
-  <label for="password" class="block text-sm font-medium text-gray-700">
-  </label>
-  <div class="mt-1">
-    <input
-      id="password"
-      name="password"
-      type="password"
-      autocomplete="current-password"
-      required="true"
-      class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-    />
-  </div>
   <slot></slot>
 </template>
