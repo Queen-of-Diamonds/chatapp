1c6717888fda02167d8921206838206867a98ed0
Christie Maduna <ferggiee@gmail.com>
2021-12-03 14:06:29 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,3 @@
-<script setup>
-import BaseButton from "@/components/baseComponents/BaseButton.vue";
-import BaseTextInput from "@/components/baseComponents/BaseTextInput.vue";
-
-const signIn = () => {
-  console.log("Want to sign in");
-};
-
-// const email = () => {
-//   console.log("enter email address");
-// };
-
-// const password = () => {
-//   console.log("enter password");
-// };
-</script>
-
 <template>
   <div class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
@@ -31,24 +14,17 @@ const signIn = () => {
     <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
         <form class="space-y-6" action="#" method="POST">
-          <div>
-            <base-text-input inputType="email" autocomplete="email" required
-              >email</base-text-input
-            >
-            <!-- <label for="email" class="block text-sm font-medium text-gray-700">
-              Email address
-            </label>
-            <div class="mt-1">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autocomplete="email"
-                required="true"
-                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              />
-            </div> -->
+          <div
+            class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          >
+            <text-input label="Email" type="email" class="mt-1"></text-input>
+            <text-input
+              label="Password"
+              type="password"
+              class="mt-1"
+            ></text-input>
           </div>
+
           <div class="flex items-center justify-between">
             <div class="flex items-center">
               <input
@@ -73,25 +49,14 @@ const signIn = () => {
           </div>
 
           <div>
-            <base-button @click="signIn"> Sign In </base-button>
-            <!-- <button
-              @click="signIn"
-              class="w-1/2 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Sign in
-            </button> -->
-          </div>
-
-          <div>
             <button
               type="submit"
-              class="w-1/2 flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              Sign up
+              Sign in
             </button>
           </div>
         </form>
-
         <div class="mt-6">
           <div class="relative">
             <div class="absolute inset-0 flex items-center">
---
+++ src/components/ExampleComponent.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="max-w-lg mx-auto my-12">
+    <!--        examples with lables -->
+    <div
+      class="border-l-4 border-blue-500 p-3 rounded mb-6 shadow bg-gray-100 font-semibold text-gray-600 tracking-wide text-lg"
+    >
+      Input components with labels
+    </div>
+    <text-input label="Email" class="mb-4"></text-input>
+    <text-input label="Password" type="password" class="mb-4"></text-input>
+    <!--        examples with label ended -->
+    <!--        examples with icons -->
+
+    <div
+      class="border-l-4 border-blue-500 p-3 rounded my-10 mb-6 shadow bg-gray-100 font-semibold text-gray-600 tracking-wide text-lg"
+    >
+      Input components with Icons
+    </div>
+
+    <text-input class="mb-4" with-icon placeholder="Enter your email...">
+      <template #icon>
+        <svg
+          class="mt-2 w-8 h-8 stroke-current text-gray-100"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          stroke="currentColor"
+        >
+          <path
+            d="M3 8L10.8906 13.2604C11.5624 13.7083 12.4376 13.7083 13.1094 13.2604L21 8M5 19H19C20.1046 19 21 18.1046 21 17V7C21 5.89543 20.1046 5 19 5H5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19Z"
+            stroke="#4A5568"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </template>
+    </text-input>
+    <text-input
+      class="mb-4"
+      type="password"
+      with-icon
+      placeholder="Enter your password..."
+    >
+      <template #icon>
+        <svg
+          class="mt-2 w-8 h-8 stroke-current text-gray-100"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          stroke="currentColor"
+        >
+          <path
+            d="M12 15V17M6 21H18C19.1046 21 20 20.1046 20 19V13C20 11.8954 19.1046 11 18 11H6C4.89543 11 4 11.8954 4 13V19C4 20.1046 4.89543 21 6 21ZM16 11V7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7V11H16Z"
+            stroke="#4A5568"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </template>
+    </text-input>
+    <!--        examples with icons ended -->
+
+    <!--        examples with error messages -->
+    <div
+      class="border-l-4 border-red-500 p-3 rounded mb-6 mt-10 shadow bg-gray-100 font-semibold text-gray-600 tracking-wide text-lg"
+    >
+      Input components with Error Messages
+    </div>
+    <text-input
+      label="Email"
+      class="mb-4"
+      :errors="errors['errorEmail']"
+      @keydown="delete errors['errorEmail']"
+    >
+    </text-input>
+    <text-input
+      label="Password"
+      type="password"
+      class="mb-4"
+      :errors="errors['errorPassword']"
+      @keydown="delete errors['errorPassword']"
+    ></text-input>
+    <!--        examples with error messages ended -->
+  </div>
+</template>
+
+<script>
+import TextInput from "@/src/Shared/TextInput.vue";
+
+export default {
+  components: {
+    TextInput,
+  },
+  data() {
+    return {
+      errorEmail: "",
+      errors: {
+        errorEmail: ["The email field is required."],
+        errorPassword: ["The password confirmation does not match."],
+      },
+    };
+  },
+};
+</script>
