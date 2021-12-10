2bd4c618a5d781e21fcc446b6153ef0e10015afc
Christie Maduna <ferggiee@gmail.com>
2021-12-10 09:48:21 +0200

    start

--- src/db.js
+++ src/db.js
@@ -1,14 +1,15 @@
-// import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 
-// const firebaseConfig = {
-//   apiKey: process.env.VUE_APP_API_KEY,
-//   authDomain: "chatapp-3ea09.firebaseapp.com",
-//   projectId: "chatapp-3ea09",
-//   storageBucket: "chatapp-3ea09.appspot.com",
-//   messagingSenderId: process.env.VUE_APP_SENDER_ID,
-//   appId: "1:98553626942:web:579b414f8380b7f495ec6f",
-// };
+const firebaseConfig = {
+    apiKey: process.env.VUE_APP_API_KEY,
+    authDomain: "chatapp-test-97b18.firebaseapp.com",
+    projectId: "chatapp-test-97b18",
+    storageBucket: "chatapp-test-97b18.appspot.com",
+    messagingSenderId: "473886046224",
+    appId: "1:473886046224:web:90b57851abd8795a50b295"
+  };
 
-// process.env.VUE_APP_API_KEY;
+  process.env.VUE_APP_API_KEY;
 
-// initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig);
+
