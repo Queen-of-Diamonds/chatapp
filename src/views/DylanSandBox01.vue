746b980a7758fc9fd9bfb4c32e83314210ce3d0b
l-k-mist <dylan@vandenbosch.co.za>
2021-12-19 16:04:58 +0200

    feature(firebase-works): add rough speech bubble

--- src/views/DylanSandBox01.vue
+++ src/views/DylanSandBox01.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="grid h-full w-full">
     <div class="header border-t-3 border-dashed border-black">One</div>
-    <div class="main border-t-3 border-dashed border-black">Two</div>
+    <div class="main border-t-3 border-dashed border-black">
+      <speech-bubble bubbleStyle="sb1" bubbleColour="#00bfb6"
+        >I'm a userz speech bubble</speech-bubble
+      >
+      <speech-bubble bubbleStyle="sb2" bubbleColour="#51884b"
+        >I'm a friend speech bubble</speech-bubble
+      >
+      <speech-bubble bubbleStyle="sb2" bubbleColour="#51884b"
+        >I'm a friend speech bubble</speech-bubble
+      >
+      <speech-bubble bubbleStyle="sb1" bubbleColour="#00bfb6"
+        >I'm a userz speech bubble</speech-bubble
+      >
+    </div>
     <div class="sidebar border-t-3 border-dashed border-black">Three</div>
     <div class="footer border-t-3 border-dashed border-black">
       <base-button @click="createMessage">test db add</base-button>
@@ -11,12 +24,14 @@
 
 <script setup>
 import { onMounted } from "vue";
-import { ref, set, get, onValue } from "firebase/database";
+import SpeechBubble from "@/components/shared/SpeechBubble.vue"
 
+import { ref, set, get, onValue } from "firebase/database";
 import db from "@/db.js";
 
+
 let count = 0;
-async function createMessage({ id, message="Automessage here" }) {
+async function createMessage({ id, message = "Automessage here" }) {
   const now = new Date();
   count++;
   /**
@@ -37,12 +52,8 @@ onValue(messagesRef, (snapshot) => {
   console.log("dvdb - onValue - data", data);
 });
 
-
 onMounted(async () => {
   // TODO Dylan: flesh this out into
-
-
-
   // get(messageRef)
   // get(child(messageRef, `users/${userId}`)).then((snapshot) => {
   //   if (snapshot.exists()) {
