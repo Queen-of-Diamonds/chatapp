3df4a0d44a34d1af2f3a5116c11d9ae23659fc36
Christie Maduna <ferggiee@gmail.com>
2021-12-21 15:37:20 +0200

    commit

---
+++ src/views/ChristieSandbox.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="container"> 
+  <div class="input">
+    <input type="text" placeholder="text" />
+    </div>
+  <div class="button">
+    <button>Click Me</button>
+  </div>
+  </div>
+</template>
+
+
+<style>
+.container {  
+width: 300px;  
+height: 300px;    
+display: grid;  
+}  
+
+.input {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 8px;
+  margin-top: 10px;
+}
+
+.button {
+  background-color: #FA8072;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  font-size: 20px;  
+  margin: auto;  
+  padding: 10px;
+  border-radius: 12px;
+}
+
+.button:hover {
+  background-color: #FFA07A;
+
+}
+</style>
